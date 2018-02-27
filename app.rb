75e2b9cd5d85f4aad5ad3e1c859bde850a8aa260
Guillaume FAURE-DUMONT <fauredumont@gmail.com>
2018-02-27 20:37:34 +0100

    Separate views into specific files, creating directory on the way

--- app.rb
+++ app.rb
@@ -3,21 +3,21 @@ set :bind, '0.0.0.0'
 
 
 get '/' do
-  "Guillaume FAURE-DUMONT<br>J'aide l'équipe technique de votre startup à gagner en efficacité"
+  haml :index
 end
 
 get '/cv' do
-  "Epita<br>DEV<br>Architecte logiciel en banques<br>ZikMe.org<br>Developpement Web (Ruby on Rails)"
+  haml  :cv
 end
 
 get '/constat' do
-  "Plein de startup se créent<br>Ruby on Rails est une très bonne technologie pour les startups<br>Mais il est difficile de recruter des seniors"
+  haml  :constat
 end
 
 get '/methode' do
-  "Analyser l'existant et trouver les points qui coutent le plus cher (en temps donc en argent) et les optimiser"
+  haml  :methode
 end
 
 get '/partager' do
-  "La connaissance est une de ces rares ressources dont les échanges sont à somme positive<br>EPITA, ACD, ACU, Yaka, Ing#<br>Communauté Ruby de Paris (ParisRB, WomenOnRails, Workshop, RubyMentors)<br>ESGI"
+  haml  :partager
 end
